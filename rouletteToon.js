6a3e0db32a1a3089b3a870b36d68f8a4bee391a4
kinghong97 <hcy1997@naver.com>
2021-09-16 17:47:54 +0900

    html 수정

--- rouletteToon.js
+++ rouletteToon.js
@@ -170,17 +170,20 @@ reader.onload = function (e) {
 
     document.write(`<nav class="navbar navbar-expand-lg navbar-light fw-bold">
     <div class="container-fluid">
-      <a class="navbar-brand navpd" href="https://troulette.netlify.app/">투네이션 룰렛 결산하기</a>
+      <a class="navbar-brand navpd" href="#">투네이션 룰렛 결산하기</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link"  href="https://troulette.netlify.app/">투네이션 룰렛 결산하기</a>
+            <a class="nav-link"  href="#">투네이션 룰렛 결산하기</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/twip">트윕 룰렛 결산하기</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://github.com/kinghong97">개발자</a>
           </li>
         </ul>
       </div>
